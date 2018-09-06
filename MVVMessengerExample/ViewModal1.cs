4990089b6af81ff6970d48bccdcea2e74992468d
uvclab <uvclab@users.noreply.github.com>
2018-09-06 13:35:32 +0900

    초기 커밋.

---
+++ MVVMessengerExample/ViewModal1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.Mvvm;
+using System.Windows.Input;
+
+namespace MVVMessengerExample
+{
+    public class ViewModal1
+    {
+        public ViewModal1()
+        {
+            Name = "I m sender";
+            SendMessageCommand = new DelegateCommand<string>(OnMessageSent);
+        }
+
+        public ICommand SendMessageCommand { get; set; }
+
+        public string Name { get; set; }
+
+        public void OnMessageSent(string s)
+        {
+            Messenger.Default.Send<string>(s);
+        }
+
+    }
+}
---
+++ MVVMessengerExample/ViewModal2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using DevExpress.Mvvm;
+
+namespace MVVMessengerExample
+{
+    public class ViewModal2 : INotifyPropertyChanged
+    {
+        public ViewModal2()
+        {
+            Name = "just initialized";
+            Messenger.Default.Register<string>(this, OnMessageUpdate);
+        }
+
+        private string name;
+        public string Name { get { return name;  } set { name = value; OnPropertyChanged("Name"); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        //https://docs.microsoft.com/en-us/dotnet/framework/wpf/data/how-to-implement-property-change-notification
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public void OnMessageUpdate(string Name)
+        {
+            this.Name = Name;
+        }
+    }
+}
